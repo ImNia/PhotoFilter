9863e8a2eeec56310b7093ae41e50d82df47faf0
ImNia <agnia-kimi1996@mail.ru>
2023-06-15 19:01:39 +0700

    Added filters

--- app/src/main/java/com/delirium/photofilter/EditorActivity.kt
+++ app/src/main/java/com/delirium/photofilter/EditorActivity.kt
@@ -7,16 +7,16 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.ui.graphics.asImageBitmap
-import com.delirium.photofilter.ui.editor.Editor
+import com.delirium.photofilter.ui.editor.FilterEditorView
 import com.delirium.photofilter.ui.theme.PhotoFilterTheme
-import java.io.FileNotFoundException
-
+import com.uvstudio.him.photofilterlibrary.PhotoFilter
 
 class EditorActivity() : ComponentActivity() {
+    private val photoFilter = PhotoFilter()
+    private var photoFilterList: List<Pair<Int, Bitmap>> = listOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val intent = intent
         val uri = Uri.parse(intent.getStringExtra("URI_IMAGE"))
 
@@ -25,9 +25,15 @@ class EditorActivity() : ComponentActivity() {
                 resources,
                 R.drawable.image
             )
+
+        photoFilterList = createPhotoFilterList(photoFilter, image)
+        editorScreen(image)
+    }
+
+    private fun editorScreen(image: Bitmap) {
         setContent {
             PhotoFilterTheme() {
-                Editor(image = image.asImageBitmap(), this)
+                FilterEditorView(image = image, filters = photoFilterList, view = this)
             }
         }
     }
@@ -36,12 +42,41 @@ class EditorActivity() : ComponentActivity() {
         finish()
     }
 
+    fun changeFilter(index: Int) {
+        editorScreen(photoFilterList.get(index).second)
+    }
+
+    private fun createPhotoFilterList(
+        photoFilter: PhotoFilter,
+        image: Bitmap
+    ): List<Pair<Int, Bitmap>> {
+        return listOf(
+            Pair(ONE, photoFilter.one(this, image)),
+            Pair(TWO, photoFilter.two(this, image)),
+            Pair(THREE, photoFilter.three(this, image)),
+            Pair(FOUR, photoFilter.four(this, image)),
+            Pair(FIVE, photoFilter.five(this, image)),
+            Pair(SIX, photoFilter.six(this, image)),
+            Pair(SEVEN, photoFilter.seven(this, image)),
+            Pair(EIGHT, photoFilter.eight(this, image)),
+            Pair(NINE, photoFilter.ten(this, image))
+        )
+    }
+
     private fun getContactBitmapFromURI(context: Context, uri: Uri?): Bitmap? {
-        try {
-            val input = context.contentResolver.openInputStream(uri!!) ?: return null
-            return BitmapFactory.decodeStream(input)
-        } catch (e: FileNotFoundException) {
-        }
-        return null
+        val input = context.contentResolver.openInputStream(uri!!) ?: return null
+        return BitmapFactory.decodeStream(input)
+    }
+
+    companion object {
+        const val ONE = 0
+        const val TWO = 1
+        const val THREE = 2
+        const val FOUR = 3
+        const val FIVE = 4
+        const val SIX = 5
+        const val SEVEN = 6
+        const val EIGHT = 7
+        const val NINE = 8
     }
 }
--- app/src/main/java/com/delirium/photofilter/ui/editor/EditorView.kt
+++ app/src/main/java/com/delirium/photofilter/ui/editor/EditorView.kt
@@ -1,15 +1,18 @@
 package com.delirium.photofilter.ui.editor
 
+import android.graphics.Bitmap
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -18,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.delirium.photofilter.EditorActivity
@@ -66,6 +70,47 @@ fun Editor(image: ImageBitmap, view: EditorActivity) {
                     .weight(1f)
             ) {
                 Text(text = "Save Photo")
+            }
+        }
+    }
+}
+
+@Composable
+fun FilterEditorView(image: Bitmap, filters: List<Pair<Int, Bitmap>>, view: EditorActivity) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundScreenColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            bitmap = image.asImageBitmap(),
+            contentDescription = "",
+            contentScale = ContentScale.Inside,
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(3f)
+                .border(BorderStroke(1.dp, Color.Black))
+                .background(BackgroundScreenColor)
+        )
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            items(filters.size) {
+                Column {
+                    Image(
+                        bitmap = filters.get(it).second.asImageBitmap(),
+                        contentDescription = null,
+                        modifier = Modifier.clickable {
+                            view.changeFilter(filters.get(it).first)
+                        }
+                    )
+                }
             }
         }
     }
